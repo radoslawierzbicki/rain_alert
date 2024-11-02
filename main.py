3acf0eaf6bcf2f6a6c516a76f78aab5430b4dbb9
Radek Wierzbicki <radoslawierzbicki34@gmail.com>
2024-11-02 21:15:14 +0100

    add account_sid = os.getenv("ACCOUNT_SID") auth_token = os.getenv("AUTH_TOKEN")

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import requests
 from dotenv import load_dotenv
 import os
+from twilio.rest import Client
 
 load_dotenv()  # Ładuje zmienne środowiskowe z pliku .env
 api_key = os.getenv("API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
 
 OMW_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 weather_params ={
     "appid": api_key,
-    "q": "Gdańsk", "lat": 54.352024, "lon": 18.646639}
+    "lat": 61.664837, "lon": 6.816487,
+    "cnt": 4}
 
 response = requests.get(OMW_Endpoint, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to take an umbrella!",
+        from_='+18594487670',
+        to='+48 786 818 893'
+    )
+    print(message.status)
